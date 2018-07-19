b80a275eabcbd77ef03bf69435a15bdaac5bd886
767889590 <39077804+767889590@users.noreply.github.com>
2018-07-19 11:05:03 +0800

    测试文件1

--- parent/test-service/src/main/java/com/galasys/test/Test3.java
+++ parent/test-service/src/main/java/com/galasys/test/Test3.java
@@ -3,12 +3,5 @@ package com.galasys.test;
 public class Test3 {
 	public void hello() {
 		System.out.println("hello test-service4");
-		System.out.println("hello test-service4");
-		System.out.println("hello test-service4");
-		System.out.println("hello test-service3");
-		System.out.println("hello test-service3");
-		System.out.println("hello test-service3");
-		System.out.println("hello test-service3");
-		System.out.println("hello test-service3");
 	}
 }
