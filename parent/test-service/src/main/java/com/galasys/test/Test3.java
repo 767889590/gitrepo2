cea2172e9de08566b08e9e9ea3affae23e2985ed
767889590 <39077804+767889590@users.noreply.github.com>
2018-07-19 10:58:20 +0800

    修改test3

--- parent/test-service/src/main/java/com/galasys/test/Test3.java
+++ parent/test-service/src/main/java/com/galasys/test/Test3.java
@@ -3,5 +3,7 @@ package com.galasys.test;
 public class Test3 {
 	public void hello() {
 		System.out.println("hello test-service3");
+		System.out.println("hello test-service3");
+		System.out.println("hello test-service3");
 	}
 }
