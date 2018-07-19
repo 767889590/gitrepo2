aedc14421845d52dd12b46196339f7fa00cb56de
767889590 <39077804+767889590@users.noreply.github.com>
2018-07-19 09:38:28 +0800

    Update Test1.java

--- parent/test-service/src/main/java/com/galasys/test/Test1.java
+++ parent/test-service/src/main/java/com/galasys/test/Test1.java
@@ -3,6 +3,6 @@ package com.galasys.test;
 public class Test1 {
 	public void hello() {
 		System.out.println("hello world");
-		System.out.println("hello world3");
+		System.out.println("hello world4");
 	}
 }
