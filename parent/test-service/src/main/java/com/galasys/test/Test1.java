133dc4fe52ec9ecb7c273443663ec75b8c198558
zhanyayun <yayun.zhan@galasys.cn>
2018-07-19 10:25:18 +0800

    提交新的资料

--- parent/test-service/src/main/java/com/galasys/test/Test1.java
+++ parent/test-service/src/main/java/com/galasys/test/Test1.java
@@ -2,8 +2,9 @@ package com.galasys.test;
 
 public class Test1 {
 	public void hello() {
-		System.out.println("hello world");
-		System.out.println("hello world5");
+		System.out.println("hello world1");
+		System.out.println("hello world2");
+		System.out.println("hello world3");
 		System.out.println("hello world4");
 	}
 }
