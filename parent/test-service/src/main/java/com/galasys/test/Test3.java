41f2cafed0277625c4234fc850ea7a9f0028b27f
zhanyayun <yayun.zhan@galasys.cn>
2018-07-19 11:01:51 +0800

    解决冲突1

--- parent/test-service/src/main/java/com/galasys/test/Test3.java
+++ parent/test-service/src/main/java/com/galasys/test/Test3.java
@@ -2,6 +2,11 @@ package com.galasys.test;
 
 public class Test3 {
 	public void hello() {
+		System.out.println("hello test-service4");
+		System.out.println("hello test-service4");
+		System.out.println("hello test-service4");
+		System.out.println("hello test-service3");
+		System.out.println("hello test-service3");
 		System.out.println("hello test-service3");
 	}
 }
