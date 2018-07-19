bc842f96567ed5fa0b73973aedc7ee7724052c27
767889590 <39077804+767889590@users.noreply.github.com>
2018-07-19 10:40:39 +0800

    测试测试

--- parent/test-service/src/main/java/com/galasys/test/Test1.java
+++ parent/test-service/src/main/java/com/galasys/test/Test1.java
@@ -2,11 +2,11 @@ package com.galasys.test;
 
 public class Test1 {
 	public void hello() {
-		System.out.println("hello world2");
-		System.out.println("hello world2");
-		System.out.println("hello world2");
-		System.out.println("hello world2");
-		System.out.println("hello world2");
-		System.out.println("hello world2");
+		System.out.println("hello world1");
+		System.out.println("hello world1");
+		System.out.println("hello world1");
+		System.out.println("hello world1");
+		System.out.println("hello world1");
+		System.out.println("hello world1");
 	}
 }
