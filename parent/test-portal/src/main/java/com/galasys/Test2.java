4ad50fb43a506cefe9d1c5c8d47f1649de918e71
zhanyayun <yayun.zhan@galasys.cn>
2018-07-19 10:07:15 +0800

    解决冲突文件

---
+++ parent/test-portal/src/main/java/com/galasys/Test2.java
@@ -0,0 +1,7 @@
+package com.galasys;
+
+public class Test2 {
+	public void sayHello() {
+		System.out.println("hello2");
+	}
+}
--- parent/test-service/src/main/java/com/galasys/test/Test1.java
+++ parent/test-service/src/main/java/com/galasys/test/Test1.java
@@ -3,6 +3,7 @@ package com.galasys.test;
 public class Test1 {
 	public void hello() {
 		System.out.println("hello world");
-		System.out.println("hello world3");
+		System.out.println("hello world5");
+		System.out.println("hello world4");
 	}
 }
