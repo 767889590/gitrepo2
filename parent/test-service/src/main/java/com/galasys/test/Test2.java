b9a827d91b0db3a3559919ea9e2ff249643312ba
zhanyayun <yayun.zhan@galasys.cn>
2018-07-19 10:55:41 +0800

    提交信息

--- parent/test-service/src/main/java/com/galasys/test/Test2.java
+++ parent/test-service/src/main/java/com/galasys/test/Test2.java
@@ -2,6 +2,8 @@ package com.galasys.test;
 
 public class Test2 {
 	public void hello() {
-		System.out.println("hello test-service");
+		System.out.println("hello test-service345");
+		System.out.println("hello test-service1");
+		System.out.println("hello test-service2");
 	}
 }
