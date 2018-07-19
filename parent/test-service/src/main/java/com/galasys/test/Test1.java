56cf3055fda255047febc5acc93344c28500c562
767889590 <39077804+767889590@users.noreply.github.com>
2018-07-19 10:35:35 +0800

    Update Test1.java
    测试2

--- parent/test-service/src/main/java/com/galasys/test/Test1.java
+++ parent/test-service/src/main/java/com/galasys/test/Test1.java
@@ -2,11 +2,11 @@ package com.galasys.test;
 
 public class Test1 {
 	public void hello() {
-		System.out.println("hello world0");
 		System.out.println("hello world1");
-		System.out.println("hello world2");
-		System.out.println("hello world3");
-		System.out.println("hello world4");
-		System.out.println("hello world5");
+		System.out.println("hello world1");
+		System.out.println("hello world1");
+		System.out.println("hello world1");
+		System.out.println("hello world1");
+		System.out.println("hello world1");
 	}
 }
